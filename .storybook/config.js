02ba42170df162adcaa6f29b2509a7875727899d
Nicholas Beach <nicholascbeach@gmail.com>
2019-12-20 17:40:40 -0500

    Removed storybook stuff

--- .storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure } from "@storybook/react";
-import "../src/style/index.scss";
-
-function loadStories() {
-    function requireAll(r) { r.keys().forEach(r); }
-    requireAll(require.context("../src/component", true, /\.stories\.tsx$/));
-}
-
-configure(loadStories, module);
