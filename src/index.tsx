04f2f978bf4f634756496ebc1de1226085dd2f5b
Nicholas Beach <nicholascbeach@gmail.com>
2019-12-20 20:50:09 -0500

    Added success and failure overlays

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import {Branding} from "coolant/build/component/Branding"
-import {atTime, Build, buildFailed, BuildList, buildNowPassing, BuildStatus, onNewBuild, playSound,} from "coolant"
+import {atTime, Build, buildFailed, BuildList, buildNowPassing, BuildStatus, onNewBuild, playSound} from "coolant"
 // @ts-ignore
-import logo from "./logo.png"
+import logo from "./images/team-logo.png"
+// @ts-ignore
+import dogLaughingImage from "./images/dog-laugh.gif"
+// @ts-ignore
+import dogWithDucks from "./images/dog-with-duck.jpg"
 // @ts-ignore
 import gotDucks from "./sounds/got-ducks.wav"
 // @ts-ignore
 import dogLaugh from "./sounds/dog-laugh.wav"
 // @ts-ignore
 import dangerZone from "./sounds/danger-zone.mp3"
+import {showOverlay} from "coolant/build/effect/overlay"
 
 
 let buildPassing = BuildStatus.Failed
@@ -26,9 +31,22 @@ const buildProvider = async (): Promise<ReadonlyArray<Build>> => {
 
 atTime("15:49", () => playSound(dangerZone))
 
+const failureOverlay = <div style={{ display: "flex",  height: "100%", alignItems: "flex-end", justifyContent: "center" }}>
+    <img src={dogLaughingImage} style={{height: "70%"}}/>
+</div>
+const successOverlay = <div style={{ display: "flex",  height: "100%", alignItems: "flex-end", justifyContent: "center" }}>
+    <img src={dogWithDucks} style={{height: "100%"}}/>
+</div>
+
 onNewBuild(buildProvider, (priorBuild, currentBuild) => {
-    if (buildNowPassing(priorBuild, currentBuild)) { playSound(gotDucks) }
-    if (buildFailed(currentBuild)) { playSound(dogLaugh) }
+    if (buildNowPassing(priorBuild, currentBuild)) {
+        playSound(gotDucks)
+        showOverlay(3, successOverlay)
+    }
+    if (buildFailed(currentBuild)) {
+        playSound(dogLaugh)
+        showOverlay(3, failureOverlay)
+    }
 })
 
 ReactDOM.render(<>
