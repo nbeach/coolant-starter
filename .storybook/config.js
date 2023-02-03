a2b95f33ff556d90a609d0cfe9fabd8e5f72d1fa
Nicholas Beach <nicholascbeach@gmail.com>
2023-02-02 21:38:35 -0500

    Updated dependencies

--- .storybook/config.js
+++
@@ -1,8 +0,0 @@
-import { configure } from "@storybook/react";
-
-function loadStories() {
-    function requireAll(r) { r.keys().forEach(r); }
-    requireAll(require.context("../src/component", true, /\.stories\.tsx$/));
-}
-
-configure(loadStories, module);
